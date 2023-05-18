d100c0954ccc7efe525c01499b701568aded75e4
Ramprit Sahani <rampritsahani@outlook.in>
2023-05-18 08:16:19 +0530

    First commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,31 @@
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+
+import { api } from "@/utils/api";
+
+import "@/styles/globals.css";
+import Head from "next/head";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Head>
+        <title>Tweeter clone</title>
+        <meta
+          name="description"
+          content="This is tweeter colone application by Ramprit Sahani"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto flex">
+        <Component {...pageProps} />
+      </div>
+    </SessionProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
